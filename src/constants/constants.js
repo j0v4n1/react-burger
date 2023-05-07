835888ea9bb32ae7638669c26ae4602319a19b82
Ivan Klimovskiy <klimoffsky2707@yandex.ru>
2023-05-07 20:01:43 +0300

    init

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,6 +2,4 @@ export const getIngredientsURL =
   "https://norma.nomoreparties.space/api/ingredients";
 export const getOrderNumberURL = "https://norma.nomoreparties.space/api/orders";
 export const getRegistrationTokenURL = "https://norma.nomoreparties.space/api/auth/register"
-export const bun = "bun";
-export const sauce = "sauce";
-export const main = "main";
+
